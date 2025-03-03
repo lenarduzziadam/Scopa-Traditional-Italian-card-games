55d1d899a39dc7e73fad3fb0a2b0b1cd6bdc0c1f
lenarduzziadam <lenarduzziadam@gmail.com>
2025-03-02 23:47:17 -0600

    experimenting with adding dictionary will need some kinks worked out

--- Cards.py
+++ Cards.py
@@ -1,15 +1,24 @@
 class Cards:
     def create_deck(self):
         cards = []
+        dictionary = {}
         suits = ["swords", "dinero", "clubs", "cups"]
         nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         
         for num in nums:
             for suit in suits:
-                cards.append(str(num) + " of " + suit)
+               cards.append(str(num) + " of " + suit)
+               
+            for i in range(len):
+               dictionary[num] = suit
+        
+             
+        
+               
         print(cards)
         print(len(cards))
-        return cards
+        print(dictionary)
+        return cards, dictionary
 
 # Create an instance of Cards and call the method
 deck = Cards()
